ec480cc8575574b41f7e308b28717a1f57cbe90d
Marikuana <marians22092000@gmail.com>
2023-03-17 09:57:44 +0200

    Update TextMessageModel

--- Server/TextMessageModel.cs
+++ Server/TextMessageModel.cs
@@ -1,6 +1,14 @@
 ﻿public class TextMessageModel
 {
+    public int Length => Text.Length;
     public string Text { get; set; }
+
+    public void Reverse()
+    {
+        char[] chars = Text.ToCharArray();
+        Array.Reverse(chars);
+        Text = new string(chars);
+    }
 
     public override string ToString()
     {
